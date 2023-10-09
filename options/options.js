6a7a3b5d2581e4cc028ed314a0d917da0cff79b7
Amalia Litsa <litsa@crowdbotics.com>
2023-10-09 08:59:54 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "crowdbotics-test-ap-43772",
-  url: "https://crowdbotics-test-ap-43772.botics.co",
-  api: "https://crowdbotics-test-ap-43772.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "crowdbotics-test-ap-43772",
+  "url": "https://crowdbotics-test-ap-43772.botics.co",
+  "api": "https://crowdbotics-test-ap-43772.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "billing"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
